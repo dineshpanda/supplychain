4ae4ed796dedf4793fadac77545e305b47f28a25
Dinesh Panda <dineshpanda92@gmail.com>
2020-05-08 00:51:49 +0530

    Only account user can modify account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,14 @@
 class AccountsController < ApplicationController
+  before_action :current_supplier_must_be_account_supplier, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_supplier_must_be_account_supplier
+    account = Account.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_supplier == account.supplier
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @accounts = Account.all
 
